f03617876573a73099f5b3d0dd563340ea0dc1dc
MollyERG <mollymaloun@gmail.com>
2024-03-04 22:58:30 +0300

    feat: Add method for extract album data

--- BCDownloader/BCDownloader.cs
+++ BCDownloader/BCDownloader.cs
@@ -1,36 +1,66 @@
 ﻿using BCDownloader.Models;
 using HtmlAgilityPack;
 using Newtonsoft.Json;
+using System.Collections.Concurrent;
 
 namespace BCDownloader
 {
-    ////*[@class="track-title"]
-    public static class BCDownloader
+    public class BCDownloader
     {
         public static HttpClient client = new();
-        public static async Task<string> GetDocumentAsync(string url)
+
+        public static async Task<IEnumerable<Trackinfo>> GetTracksAsync(string url)
+        {
+            var document = await GetDocumentAsync(url);
+            var albumInfo = GetAlbumInfo(document);
+
+            var options = new ParallelOptions
+            {
+                MaxDegreeOfParallelism = 1000,
+                CancellationToken = default
+            };
+
+            if (albumInfo?.TrackInfo != null)
+            {
+                var data = new ConcurrentBag<Trackinfo>();
+
+                await Parallel.ForEachAsync(albumInfo.TrackInfo.ToList(), options, async (trackInfo, ct) =>
+                {
+                    var response = await client.GetAsync(trackInfo.File?.downloadPath);
+                    trackInfo.Data = await response.Content.ReadAsByteArrayAsync();
+                    data.Add(trackInfo);
+                });
+
+                return data;
+            }
+
+            return null;
+        }
+
+        private static async Task<string> GetDocumentAsync(string url)
         {
             var response = await client.GetAsync(url);
             return await response.Content.ReadAsStringAsync();
         }
+
         public static IEnumerable<string> GetTrackList(string document)
         {
             var doc = new HtmlDocument();
             doc.LoadHtml(document);
-            var track = doc.DocumentNode.SelectNodes("//*[@class=\"track-title\"]").Select(x => x.GetDirectInnerText()).ToList();
+            var track = doc.DocumentNode.SelectNodes("//*[@class=\"track-title\"]").Select(x => x.GetDirectInnerText());
             return track;
         }
 
-        public static async Task<AlbumInfo?> GetAlbumInfoAsync(string document)
+        private static AlbumInfo? GetAlbumInfo(string document)
         {
             var doc = new HtmlDocument();
             doc.LoadHtml(document);
-            var albumInfo = await Task.Run(() => JsonConvert.DeserializeObject<AlbumInfo>(doc.DocumentNode
+            var albumInfo = JsonConvert.DeserializeObject<AlbumInfo>(doc.DocumentNode
                 .SelectNodes("//script")
                 .Where(x => x.Attributes.Contains("data-tralbum"))
                 .FirstOrDefault()
                 ?.GetAttributeValue("data-tralbum", "1")
-                .Replace("&quot;", "\"") ?? string.Empty));
+                .Replace("&quot;", "\"") ?? string.Empty);
 
             return albumInfo;
         }
@@ -41,18 +71,10 @@ namespace BCDownloader
             return await response.Content.ReadAsByteArrayAsync();
         }
 
-        public static async Task GetFileAsync(string url)
+        public static async Task<byte[]> GetFileAsync(Trackinfo info)
         {
-            var albumInfo = await GetAlbumInfoAsync(url);
-
-            if (albumInfo?.TrackInfo != null)
-            {
-                foreach (var trackInfo in albumInfo.TrackInfo)
-                {
-
-                }
-            }
-
+            var response = await client.GetAsync(info.File.downloadPath);
+            return await response.Content.ReadAsByteArrayAsync();
         }
     }
 }
