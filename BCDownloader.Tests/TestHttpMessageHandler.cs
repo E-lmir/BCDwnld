15005f827b06da362a90c3d4408fe69bde3d90fb
E-lmir <mollymaloun@gmail.com>
2024-07-08 23:46:39 +0400

    Add tests

---
+++ BCDownloader.Tests/TestHttpMessageHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BCDownloader.Tests
+{
+    internal class TestHttpMessageHandler (string document) : HttpMessageHandler
+    {
+        private readonly string _document = document;
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var response = new HttpResponseMessage()
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent(_document, Encoding.UTF8),
+            };
+
+            return Task.FromResult(response);
+        }
+    }
+}
