a950fee598dcf8fec08077935035a39eeb01bd2f
E-lmir <mollymaloun@gmail.com>
2024-10-15 21:35:13 +0400

    Add title property to AlbumInfo model

--- BCDownloader/Models/AlbumInfo.cs
+++ BCDownloader/Models/AlbumInfo.cs
@@ -6,6 +6,8 @@ namespace BCDownloader.Models
     public record AlbumInfo(
         [JsonProperty("artist")]
         string? Artist,
+        [JsonProperty("title")]
+        string? Title,
         [JsonProperty("album_release_date")]
         string? ReleaseDate,
         [JsonProperty("trackinfo")]
